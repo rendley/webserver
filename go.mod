531921e3237a323aa44c6546551b02dc84d6b713
rendle <rendle@24h.tv>
2024-10-27 23:22:34 +0300

    add logrus log package

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/rendley/webserver
 
 go 1.23.0
 
-require github.com/BurntSushi/toml v1.4.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
--- internal/app/api/api.go
+++ internal/app/api/api.go
@@ -1,16 +1,25 @@
 package api
 
+import "github.com/sirupsen/logrus"
+
 type API struct {
 	// unexported field
 	config *Config
+	logger *logrus.Logger
 }
 
 func New(config *Config) *API {
 	return &API{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 func (api *API) Start() error {
+	// Trying to configure Logger
+	if err := api.configureLoggerField(); err != nil {
+		return err
+	}
+	api.logger.Info("starting api server at port:", api.config.BindAddr)
 	return nil
 }
